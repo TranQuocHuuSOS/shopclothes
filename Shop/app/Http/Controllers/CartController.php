2d2e18697cb7711827d5b679527e07b8c5acca4a
Thuhuong050903 <huong.phan24@student.passerellesnumeriques.org>
2023-06-23 19:33:32 +0700

    Xong show sản phẩm

--- Shop/app/Http/Controllers/CartController.php
+++ Shop/app/Http/Controllers/CartController.php
@@ -2,9 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Carts;
+use App\Models\Products;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
 
 class CartController extends Controller
 {
     //
+    public function getAddToCart(Request $req, $id)
+    {
+        if (Session::has('user')) {
+            if (Products::find($id)) {
+                $product = Products::find($id);
+                $oldCart = Session('cart') ? Session::get('cart') : null;
+                $cart = new Carts($oldCart);
+                $cart->add($product, $id);
+                $req->session()->put('cart', $cart);
+                return redirect()->back();
+            } else {
+                return '<script>alert("Không tìm thấy sản phẩm này.");window.location.assign("/");</script>';
+            }
+        } else {
+            return '<script>alert("Vui lòng đăng nhập để sử dụng chức năng này.");window.location.assign("/login");</script>';
+        }
+    }
 }
--- Shop/app/Http/Controllers/PageController.php
+++ Shop/app/Http/Controllers/PageController.php
@@ -2,9 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Products;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
     //
+    public function getIndex()
+    {
+        $products = Products::all();
+    
+        return view('shop', ['products' => $products]);
+    }
+    
 }
